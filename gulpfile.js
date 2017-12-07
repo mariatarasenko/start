34ddaaf9109c25bf5bf31337c174ac5ba48694bc
Maria Tarasenko <mariaatarasenko@gmail.com>
2017-12-08 01:28:03 +0300

    qulp project done

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,18 @@ const gulp = require('gulp'),
       autoprefixer = require('gulp-autoprefixer'),
       sourceMap = require('gulp-sourcemaps'),
       del = require('del'),
-      browserSync = require('browser-sync').create();
+      gulpWebpack = require('gulp-webpack'),
+      webpack= require('webpack'),
+      webpackConfig = require('./webpack.config.js'),
+      browserSync = require('browser-sync').create(),
+      plumber = require('gulp-plumber'),
+      rename = require('gulp-rename'),
+      sassGlob = require('gulp-sass-glob'),
+      groupCssMedia = require('gulp-group-css-media-queries'),
+      imagemin = require('gulp-imagemin'),
+      svgSprite = require('gulp-svg-sprite')
+      injectGulp = require('gulp-inject')
+      ;
 
 
 const paths = {
@@ -14,39 +25,71 @@ const paths = {
         src:'src/templates/**/*.pug',
     },
     styles:{
-            src:'src/styles/**/*.scss',
+            src:'src/styles/**/**/*.scss',
             dest: 'build/assets/styles/'
     },
     images:{
         src: 'src/images/**/*.*',
         dest: 'build/assets/images'
+    },
+    scripts: {
+        src: 'src/js/**/*.js',
+        dest: 'build/assets/js/'
+    },
+    svg: {
+        src: 'src/svg/**/*.svg',
+        dest: 'build/assets/images/svg/'
     }
 
 }
 
 //pug
-function templates(){
+function templates() {
     return gulp.src(paths.templates.pages)
+    
+
+    .pipe(plumber())
     .pipe(pug({pretty:true}))
     .pipe(gulp.dest(paths.root));
 }
+
+// function inject(){
+//     return gulp.src('./src/templates/template.pug')
+//     var sources = gulp.src(['./build/assets/js/*.js', './build/assets/styles/*.min.css'])
+//     .pipe(injectGulp(sources))
+//     .pipe(gulp.dest(paths.root));
+// }
 
 //scss
 
 function styles() {
     return gulp.src('./src/styles/main.scss')
+        .pipe(plumber())
         .pipe(sourceMap.init())
+        .pipe(sassGlob())
         .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(groupCssMedia())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
         .pipe(sourceMap.write())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest(paths.styles.dest))
 }
 
+//clean
 function clean(){
     return del(paths.root);
+}
+
+
+// webpack
+function scripts() {
+    return gulp.src('src/js/app.js')
+        .pipe(plumber())
+        .pipe(gulpWebpack(webpackConfig, webpack)) 
+        .pipe(gulp.dest(paths.scripts.dest));
 }
 
 
@@ -55,6 +98,7 @@ function watch() {
     gulp.watch(paths.styles.src, styles);
     gulp.watch(paths.templates.src, templates);
     gulp.watch(paths.images.src, images);
+    gulp.watch(paths.scripts.src, scripts);
 }
 
 //browserSync
@@ -68,8 +112,25 @@ function server(){
 //remove images
 function images(){
     return gulp.src(paths.images.src)
+        .pipe(imagemin())
         .pipe(gulp.dest(paths.images.dest));
         //добавить сжатиье картинок 
+}
+const configur					= {
+    shape				: {
+        dimension		: {		
+            maxWidth	: 32,
+            maxHeight	: 32
+        },
+        spacing			: {			
+            padding		: 10
+        }
+    }
+};
+
+function svg(){
+    return gulp.src(paths.svg.src)
+        .pipe(svgSprite(configur))
 }
 
 exports.templates = templates;
@@ -78,11 +139,16 @@ exports.clean = clean;
 exports.watch = watch;
 exports.server = server;
 exports.images = images;
+exports.scripts = scripts;
+exports.svg = svg;
+//exports.inject = inject;
+
 
 
 
 gulp.task('default',gulp.series(
     clean,
-    gulp.parallel(styles, templates, images),
+    gulp.parallel(styles, templates, images, scripts),
+   
     gulp.parallel(watch,server)
 ));
